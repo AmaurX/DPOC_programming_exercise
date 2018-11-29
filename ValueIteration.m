20ecfc1897845a557dd6418353f7adbeffc923bf
nicholaspalomo <npalomo@student.ethz.ch>
2018-11-29 01:46:52 +0100

    Code runs but results not quite right yet

--- ValueIteration.m
+++ ValueIteration.m
@@ -35,12 +35,49 @@ function [ J_opt, u_opt_ind ] = ValueIteration( P, G )
 % J_k+1 (i) min_(u in U(i)) {g(i, u) + sum_(j=1)^n p_ij (u) J_k (i)}
 
 err = 1e-100;
+K = length(G(:,1));
+J_kplus1 = ones(K, 1);
+J_k = zeros(K, 1);
+U_optimal = zeros(K,1);
 
-J = zeros(K, K, L); % indx 1:2 - state(x, y), indx 3 - input u 
+%%
+% state_label_mat = zeros(1,K^2);
+%     for idx = 1:K^2
+%         state_label_mat(idx) = mod(idx,K+1);
+%     end
+% state_label_mat = reshape(state_label_mat,[K,K])';
+% state_label_mat = triu(state_label_mat) + triu(state_label_mat)' - eye(K,K);
+%%
+cost_sum = 0;
+cost_to_go_candidates = zeros(1,length(P(1,1,:)));
+input_candidates = zeros(1,length(P(1,1,:)));
+test = 0;
 
-    g_iu = G(i, u);
-
-    p_ij = P(i, j, u);
+while test == 0               
+    for current_state_row = 1:K
+        i = current_state_row;
+            for state_prob_row = 1:K
+                i_ = state_prob_row;
+                    for u = 1:length(P(1,1,:))
+                        g_iu = G(i, u);
+                            for state_prob_col = 1:K
+                                j_ = state_prob_col;
+                                p_ij_u = P(i_, j_, u);
+                                cost_sum = cost_sum + p_ij_u*J_k(j_);
+                            end
+                        cost_to_go_candidates(u) = g_iu + cost_sum;
+                        input_candidates(u) = u;
+                    end
+                J_kplus1(i_) = min(cost_to_go_candidates);
+                U_optimal(i_) = min(find(cost_to_go_candidates == min(cost_to_go_candidates)));
+            end       
+    end
+test = (abs(J_k(i_) - J_kplus1(i_)) >= err);                   
+J_k = J_kplus1;
+end
     
-    J_k = J_kplus1;
+    
+    J_opt = J_k;
+    u_opt_ind = U_optimal;
+    
 end
